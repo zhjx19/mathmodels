3aa78d2a561c829f1de631869cf1a0d8d9a5ac54
zhjx19 <zhjx_19@163.com>
2025-08-05 00:30:43 +0800

    update coupling_degree()

--- R/system_evaluation.R
+++ R/system_evaluation.R
@@ -11,6 +11,7 @@
 #' @param data A data frame with normalized scores (usually in [0,1]) as columns, .
 #' @param w Optional vector of weights for indicators or subsystems; defaults to equal weights if NULL.
 #' @param id_cols Optional character vector of column names in \code{data} to preserve as identifiers (not used in calculations).
+#' @param type Either "standard" for the standard coupling formula (results concentrated near 1) or "adjusted" for the revised formula (results more uniformly distributed in [0,1]), as proposed by Wang Shujia, Kong Wei, et al. in "Misconceptions and Corrections of Domestic Coupling Coordination Degree Models, Journal of Natural Resources, 2021, 36(3): 793–810 (In Chinese)"
 #'
 #' @return
 #' A tibble depending on the function:
@@ -35,22 +36,26 @@
 #' @examples
 #' # Sample normalized subsystem scores
 #' df = data.frame(
+#'   ID = LETTERS[1:6],
 #'   s1 = c(0.0162, 0.1782, 0.5490, 0.6730, 0.0207, 0.9875),
-#'   s2 = c(0.2720, 0.6824, 0.0593, 0.4812, 0.8891, 0.5573)
+#'   s2 = c(0.2720, 0.6824, 0.0593, 0.4812, 0.8891, 0.5573),
+#'   s3 = c(0.2655, 0.3721, 0.5729, 0.9082, 0.2017, 0.8984)
 #' )
 #' # Coupling Degree Analysis
-#' coupling_degree(df)        # Equal weights
-#' coupling_degree(df, c(0.6, 0.4))
+#' coupling_degree(df, id_cols = ID)        # Equal weights
+#' coupling_degree(df, c(0.4, 0.3, 0.3), id_cols = ID,
+#'                 type = "adjusted")
 #' # Obstacle Degree Analysis
-#' obstacle_degree(df)        # Equal weights
-#' obstacle_degree(df, c(0.6, 0.4))
+#' obstacle_degree(df, id_cols = ID)        # Equal weights
+#' obstacle_degree(df, c(0.4, 0.3, 0.3), id_cols = ID)
 #'
 #' @name system_evaluation
 NULL
 
 #' @rdname system_evaluation
 #' @export
-coupling_degree = function(data, w = NULL, id_cols = NULL) {
+coupling_degree = function(data, w = NULL, id_cols = NULL,
+                           type = "standard") {
   # Computes coupling degree(CD), coordination index(CI), and coupling coordination degree(CCD)
   # data: data frame with normalized subsystem scores as columns, Can include ID columns that do not participate in the calculation
   # w: vector of subsystem weights
@@ -65,10 +70,21 @@ coupling_degree = function(data, w = NULL, id_cols = NULL) {
 
   p = ncol(data)
   if(is.null(w)) w = rep(1/p,p)
-  row_prod = apply(data, 1, function(x) prod(pmax(x, 1e-10)))
-  row_sum = pmax(rowSums(data), 1e-10)   # Prevent division by 0
-  C = p * row_prod^(1/p) / row_sum
-  TT = as.matrix(data) %*% w |> as.vector()
+  C = switch(type,
+    "standard" = {
+       row_prod = apply(data, 1, function(x) prod(pmax(x, 1e-10)))
+       row_sum = pmax(rowSums(data), 1e-10)   # Prevent division by 0
+       p * row_prod^(1/p) / row_sum
+     },
+     "adjusted" = {
+       apply(data, 1, function(x) {
+         idx = combn(p, 2)
+         C1 = 1 - mean(abs(x[idx[2,]] - x[idx[1,]]))
+         C2 = prod(x / max(x))
+         sqrt(C1 * C2)
+       })
+     })
+  TT = as.vector(as.matrix(data) %*% w)
   D = sqrt(C * TT)
   tibble::tibble(ID, coupling = C, coord = TT, coupling_coord = D)
 }
